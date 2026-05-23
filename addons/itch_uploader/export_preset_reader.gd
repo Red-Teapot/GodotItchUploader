4fa0e8cb3ce15aeed875add60636fd51f7b24239
Nikita Medvedev <contact@nmedve.dev>
2026-05-23 16:22:35 +0400

    #6 Support Godot 4.7

---
+++ addons/itch_uploader/export_preset_reader.gd
@@ -0,0 +1,95 @@
+@tool
+extends RefCounted
+class_name ExportPresetReader
+
+const ITCH_CHANNELS := {
+	"Web": "web",
+	"Windows Desktop": "windows",
+	"macOS": "macos",
+	"Linux": "linux",
+}
+
+func read_export_presets() -> Array[ExportPreset]:
+	var export_presets := ConfigFile.new()
+	var error := export_presets.load("res://export_presets.cfg")
+	if error != OK:
+		printerr(error_string(error))
+		return []
+	
+	var godot_version := Engine.get_version_info()
+	var godot_version_number: int = godot_version.hex
+	
+	if godot_version_number < 0x040000:
+		printerr("Unsupported Godot version: " + godot_version.string)
+		return []
+	elif godot_version_number < 0x040700:
+		return _parse_export_presets_pre_4_7(export_presets)
+	else:
+		return _parse_export_presets_post_4_7(export_presets)
+
+func _parse_export_presets_pre_4_7(export_presets: ConfigFile) -> Array[ExportPreset]:
+	var presets: Array[ExportPreset] = []
+	
+	for section in export_presets.get_sections():
+		if not section.begins_with("preset."):
+			continue
+		if section.ends_with(".options"):
+			continue
+		
+		var is_runnable = export_presets.get_value(section, "runnable", false)
+		if not is_runnable:
+			continue
+		
+		var platform = export_presets.get_value(section, "platform")
+		if not platform or platform not in ITCH_CHANNELS:
+			continue
+		
+		presets.append(ExportPreset.new(
+			export_presets.get_value(section, "name"),
+			platform,
+			export_presets.get_value(section, "export_path"),
+			ITCH_CHANNELS[platform],
+		))
+	
+	return presets
+
+func _parse_export_presets_post_4_7(export_presets: ConfigFile) -> Array[ExportPreset]:
+	var sections := export_presets.get_sections()
+	
+	var presets: Array[ExportPreset] = []
+	var runnable_presets: Dictionary[String, String] = {}
+	
+	for section in sections:
+		if section != "runnable_presets":
+			continue
+		
+		var platforms := export_presets.get_section_keys(section)
+		for platform in platforms:
+			var preset_name := export_presets.get_value(section, platform)
+			if not preset_name or not preset_name is String:
+				continue
+			
+			runnable_presets[preset_name] = platform
+	
+	for section in sections:
+		if not section.begins_with("preset."):
+			continue
+		if section.ends_with(".options"):
+			continue
+		
+		var preset_name = export_presets.get_value(section, "name")
+		if not preset_name in runnable_presets:
+			continue
+		
+		var platform = export_presets.get_value(section, "platform")
+		if not platform or platform not in ITCH_CHANNELS:
+			continue
+		
+		presets.append(ExportPreset.new(
+			preset_name,
+			platform,
+			export_presets.get_value(section, "export_path"),
+			ITCH_CHANNELS[platform],
+		))
+	
+	return presets
--- addons/itch_uploader/itch_uploader.gd
+++ addons/itch_uploader/itch_uploader.gd
@@ -14,6 +14,8 @@ var _export_settings_modal: ExportSettingsModal = null
 const EXPORT_PROCESS_MODAL_RES := preload("res://addons/itch_uploader/export_process_modal/export_process_modal.tscn")
 var _export_process_modal: ExportProcessModal = null
 
+var _export_preset_reader := ExportPresetReader.new()
+
 func _enter_tree():
 	add_tool_menu_item(TOOL_MENU_ITEM_NAME, _open_export_settings_modal)
 	
@@ -38,7 +40,7 @@ func _get_setting(field: String) -> String:
 func _open_export_settings_modal():
 	_export_settings_modal = EXPORT_SETTINGS_MODAL_RES.instantiate()
 	_export_settings_modal.theme = EditorInterface.get_editor_theme()
-	_export_settings_modal.export_presets = _read_export_presets()
+	_export_settings_modal.export_presets = _export_preset_reader.read_export_presets()
 	_export_settings_modal.connect("export_accepted", self._start_export)
 	EditorInterface.popup_dialog_centered(_export_settings_modal)
 
@@ -49,42 +51,3 @@ func _start_export(selected_export_presets: Array[ExportPreset], butler_path: St
 	_export_process_modal.export_presets = selected_export_presets
 	_export_process_modal.butler_path = butler_path
 	EditorInterface.popup_dialog_centered(_export_process_modal)
-
-const ITCH_CHANNELS := {
-	"Web": "web",
-	"Windows Desktop": "windows",
-	"macOS": "macos",
-	"Linux": "linux",
-}
-
-func _read_export_presets() -> Array[ExportPreset]:
-	var export_presets := ConfigFile.new()
-	var error := export_presets.load("res://export_presets.cfg")
-	if error != OK:
-		printerr(error)
-		return []
-	
-	var presets: Array[ExportPreset] = []
-	
-	for section in export_presets.get_sections():
-		if not section.begins_with("preset."):
-			continue
-		if section.ends_with(".options"):
-			continue
-		
-		var is_runnable = export_presets.get_value(section, "runnable", false)
-		if not is_runnable:
-			continue
-		
-		var platform = export_presets.get_value(section, "platform")
-		if not platform or platform not in ITCH_CHANNELS:
-			continue
-		
-		presets.append(ExportPreset.new(
-			export_presets.get_value(section, "name"),
-			platform,
-			export_presets.get_value(section, "export_path"),
-			ITCH_CHANNELS[platform],
-		))
-	
-	return presets
